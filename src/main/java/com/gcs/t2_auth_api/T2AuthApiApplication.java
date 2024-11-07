6d7541e15a2e2a10058b9d65211b9fe535188411
Gabriel Cruz Soto <gabrielcruzsoto@gmail.com>
2024-11-07 19:36:31 -0300

    project initial

---
+++ src/main/java/com/gcs/t2_auth_api/T2AuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.gcs.t2_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2AuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2AuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcs/t2_auth_api/T2AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcs.t2_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
